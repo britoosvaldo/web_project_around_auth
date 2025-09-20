de0f0acea87c2507164d835cd7841d1e1dcc7910
Osvaldobrito <osvaldooabn@gmail.com>
2025-09-20 17:10:07 -0400

    atualização do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
