0c65d20bb508fcaed724854a6f6ffc305f5f04de
Osvaldobrito <osvaldooabn@gmail.com>
2025-09-22 11:20:14 -0400

    atualização do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/web_project_around_auth">
+    <BrowserRouter basename="/web_project_around_auth">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
